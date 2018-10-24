ff8c2f7cfd530a94c0c9909c442cd79222ad31dd
John M Naglick <jnaglick3@bloomberg.net>
2018-10-24 17:30:40 -0400

    Ember.guidFor instead of uuid()

--- addon/mixins/model.js
+++ addon/mixins/model.js
@@ -23,13 +23,6 @@ const defaultOptions = function(options) {
   }
 };
 
-const uuid = function() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-};
-
 export default Ember.Mixin.create({
   hasDirtyAttributes: Ember.computed('currentState.isDirty', 'markedForDestruction', 'markedForDeletion', function() {
     let original = this._super(...arguments);
@@ -94,7 +87,7 @@ export default Ember.Mixin.create({
 
   tempId() {
     if (!this._tempId) {
-      this._tempId = uuid();
+      this._tempId = Ember.guidFor(this);
     }
     return this._tempId;
   },
